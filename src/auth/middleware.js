1394b8f6d7da017556c0e6c856bb63804bad466c
shiratap <spencerhirata@gmail.com>
2019-04-04 15:54:58 -0700

    testing packages all hooked up

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -10,18 +10,22 @@ module.exports = (capability) => {
       let [authType, authString] = req.headers.authorization.split(/\s+/);
 
       switch (authType.toLowerCase()) {
-        case 'basic':
-          return _authBasic(authString);
-        case 'bearer':
-          return _authBearer(authString);
-        default:
-          return _authError();
+      case 'basic':
+        return _authBasic(authString);
+      case 'bearer':
+        return _authBearer(authString);
+      default:
+        return _authError();
       }
     } catch (e) {
       _authError();
     }
 
-
+    /**
+     * Changes the buffer to a username and password, checks if the user exists in database
+     * @param {String} str
+     * @returns User 
+     */
     function _authBasic(str) {
     // str: am9objpqb2hubnk=
       let base64Buffer = Buffer.from(str, 'base64'); // <Buffer 01 02 ...>
@@ -34,12 +38,20 @@ module.exports = (capability) => {
         .catch(_authError);
     }
 
+    /**
+     * Checks if user exists in database with sent token
+     * @param {object} authString 
+     */
     function _authBearer(authString) {
       return User.authenticateToken(authString)
         .then(user => _authenticate(user))
         .catch(_authError);
     }
 
+    /**
+     * 
+     * @param {object} user 
+     */
     function _authenticate(user) {
       if ( user && (!capability || (user.can(capability))) ) {
         req.user = user;
@@ -50,7 +62,9 @@ module.exports = (capability) => {
         _authError();
       }
     }
-
+    /**
+     * Error handler
+     */
     function _authError() {
       next('Invalid User ID/Password');
     }
